48516eea937f2baa05642c7dde2a969e5ef696d3
Hugh <skidoau@gmail.com>
2024-02-02 09:52:59 +0800

    Add Assigment3

--- Week3/index.js
+++ Week3/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, My Server!</h1>');
+app.get('/getData', (req, res) => {
+    const number = req.query.number
+    if ( !number ){
+        res.send ( '缺少參數' )
+    } else if ( isNaN( number ) || number <= 0 ){
+        res.send( '參數錯誤' )
+    } else {
+        const result = calculateSum ( parseInt( number ) )
+        res.send(`數字:${number},結果:${result}`)
+    }
 });
+function calculateSum ( number ){
+    let sum = 0
+    for( let i = 0; i <= number; i++ ){
+        sum += i
+    }
+    return sum
+}
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
